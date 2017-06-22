3a59f11a92e89c7bd34e5cdfa61af4b74351a1d1
keepmovinghe <chen615550721@163.com>
2017-06-22 18:38:54 +0800

    add log4j

--- src/com/keep/servlet/CoreServlet.java
+++ src/com/keep/servlet/CoreServlet.java
@@ -1,6 +1,8 @@
 package com.keep.servlet;
 
 import com.keep.utils.SignUtil;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +16,9 @@ import java.io.PrintWriter;
 public class CoreServlet extends HttpServlet {
     private static final long serialVersionUID = 7700001531328067224L;
 
+    private static final Logger logger  = LogManager.getLogger(CoreServlet.class);
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.debug("请求参数："+request);
         // 微信加密签名
         String signature = request.getParameter("signature");
         // 时间戳
@@ -24,6 +28,12 @@ public class CoreServlet extends HttpServlet {
         // 随机字符串
         String echostr = request.getParameter("echostr");
 
+        System.out.println("-------------------");
+        System.out.println("signature:"+signature);
+        System.out.println("timestamp:"+timestamp);
+        System.out.println("nonce:"+nonce);
+        System.out.println("echostr:"+echostr);
+        System.out.println("-------------------");
         // 若请求校验成功，则原样返回 echostr,表示接入成功，否则接入失败
         PrintWriter out = response.getWriter();
         if(SignUtil.checkSignature(signature,timestamp,nonce)){
--- src/com/keep/utils/SignUtil.java
+++ src/com/keep/utils/SignUtil.java
@@ -60,7 +60,7 @@ public class SignUtil {
      * @return
      */
     private static String byteToHexStr(byte ib){
-        char[] Digit = {'1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[] Digit = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         char[] ob = new char[2];
         ob[0] = Digit[(ib>>>4) & 0X0F];
         ob[1] = Digit[ib & 0X0F];
